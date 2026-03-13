ac28b385bf8f4a3610eed91b7ba75db6963d2cda
Surbhi Priya <surbhipriya152@gmail.com>
2026-03-13 14:31:56 +0530

    final change in controller

--- backend/controllers/tripController.js
+++ backend/controllers/tripController.js
@@ -5,7 +5,7 @@ const fetch = require("node-fetch");
 async function geminiGenerate(prompt) {
   try {
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" + process.env.GEMINI_API_KEY,
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=" + process.env.GEMINI_API_KEY,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
